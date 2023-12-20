f3a92d8354d0f612dad5cda812cf3db0a0360129
Bjonne <Bjonne696@gmail.com>
2023-12-20 04:53:43 +0100

    created basic JS functions for register and login

---
+++ js/login.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const API_URL = 'https://api.noroff.dev/api/v1';
+
+    function displaySuccessMessage(message) {
+        const successDiv = document.getElementById('successMessage');
+        successDiv.textContent = message;
+        successDiv.classList.remove('d-none');
+    }
+
+    function displayErrorMessage(message) {
+        const errorDiv = document.getElementById('errorMessage');
+        errorDiv.textContent = message;
+        errorDiv.classList.remove('d-none');
+    }
+
+
+    document.getElementById('loginForm').addEventListener('submit', async function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('loginNameInput').value;
+        const email = document.getElementById('loginEmailInput').value;
+        const password = document.getElementById('loginPassword').value;
+
+        try {
+            let response = await fetch(`${API_URL}/social/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            });
+
+            if (response.ok) {
+                let data = await response.json();
+                localStorage.setItem('userToken', data.accessToken);
+                displaySuccessMessage("Logged in successfully!");
+
+            } else {
+                const errorData = await response.json();
+                console.error(errorData);
+                displayErrorMessage('Error logging in. Please try again.');
+            }
+        } catch (error) {
+            console.error('There was an error logging in', error);
+            displayErrorMessage('Error logging in. Please try again.');
+        }
+    })
+});
