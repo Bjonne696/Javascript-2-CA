09fcbfa4ddafa04206fd4d77303fa90aef967835
Bjonne <Bjonne696@gmail.com>
2024-01-07 15:52:29 +0100

    everything functions

--- src/js/searchPosts.js
+++ src/js/searchPosts.js
@@ -1,9 +1,9 @@
-
-
 export function searchPosts(allPosts, searchTerm) {
-    return allPosts.filter(post =>
-        post.title.toLowerCase().includes(searchTerm) || 
-        post.body.toLowerCase().includes(searchTerm) || 
-        (post.tags && post.tags.join(' ').toLowerCase().includes(searchTerm))
-    );
+    return allPosts.filter(post => {
+        const title = post.title ? post.title.toLowerCase() : '';
+        const body = post.body ? post.body.toLowerCase() : '';
+        const tags = post.tags ? post.tags.join(' ').toLowerCase() : '';
+
+        return title.includes(searchTerm) || body.includes(searchTerm) || tags.includes(searchTerm);
+    });
 }
